6d377b203652381d4b6c8cb9615e0ef5cfc4266f
Petr Hosek <hosek.cejc@gmail.com>
2026-05-14 17:28:21 +0200

    adding L5 dockerfile

---
+++ L5/app/calculator.py
@@ -0,0 +1,22 @@
+def multiply(a, b):
+    """
+    Multiplies two numbers.
+    
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+    
+    Returns:
+        int or float: The product of a and b.
+    """
+    return a * b
+
+def main():
+    result = multiply(5, 3)
+    print(f"Výsledek: {result}")
+
+
+
+if __name__ == "__main__":
+
+    main()
