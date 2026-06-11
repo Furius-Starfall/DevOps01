5fde2503d9008e20bfca5efbb457a6aada72543c
Petr Hosek <hosek.cejc@gmail.com>
2026-06-11 18:27:23 +0200

    editing actions for L8

--- L8/terraform/main.tf
+++ L8/terraform/main.tf
@@ -160,6 +160,8 @@ resource "aws_ecs_task_definition" "lesson7" {
   cpu                      = "256"
   memory                   = "512"
 
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  
   container_definitions = jsonencode([
     {
       name      = "web"
